f37edd2e788b238c4de43a9e35ea536fd30c12f4
Jakob Braun <jakob.braun@posteo.de>
2022-05-18 05:08:16 -0300

    fixed build script

--- build.sh
+++ build.sh
@@ -4,4 +4,7 @@ pk_version=2.4.3
 mvn org.apache.maven.plugins:maven-dependency-plugin:3.1.1:get "-Dartifact=com.exasol:project-keeper-cli:$pk_version:jar"
 java -jar "$HOME/.m2/repository/com/exasol/project-keeper-cli/$pk_version/project-keeper-cli-$pk_version.jar" verify
 mvn --file server/ install
-go test -v -coverprofile=coverage.out go-client/...
+
+cd go-client
+go test -v -coverprofile=coverage.out ./...
+cd ..
