dbaa823907b96416ea09a9d7661a01aa28eab833
Christoph Pirkl <christoph.pirkl@exasol.com>
2022-05-27 11:15:59 +0200

    Add --batch-mode to build script

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ set -eo pipefail
 pk_version=2.4.4
 mvn org.apache.maven.plugins:maven-dependency-plugin:3.1.1:get "-Dartifact=com.exasol:project-keeper-cli:$pk_version:jar"
 java -jar "$HOME/.m2/repository/com/exasol/project-keeper-cli/$pk_version/project-keeper-cli-$pk_version.jar" verify
-mvn --file server/ install
+mvn --file server/ --batch-mode install
 
 cd go-client
 go test -v -coverprofile=coverage.out ./...
