6fac1cdf60e6cee9491ec3f792e315089547b7b5
Christoph Pirkl <christoph.pirkl@exasol.com>
2022-10-27 14:12:27 +0200

    Don't create an empty file when download fails

--- go-client/download.go
+++ go-client/download.go
@@ -3,11 +3,21 @@ package exasol_test_setup_abstraction_go
 import (
 	"fmt"
 	"io"
+	"log"
 	"net/http"
 	"os"
 )
 
 func downloadFile(url, localPath string) error {
+	log.Printf("Downloading %q to local path %q...", url, localPath)
+	resp, err := http.Get(url)
+	if err != nil {
+		return fmt.Errorf("download failed: %v", err.Error())
+	}
+	if resp.StatusCode != 200 {
+		return fmt.Errorf("download of %q failed with status %d: %q", url, resp.StatusCode, resp.Status)
+	}
+	defer resp.Body.Close()
 	out, err := os.Create(localPath)
 	defer func() {
 		err = out.Close()
@@ -18,14 +28,6 @@ func downloadFile(url, localPath string) error {
 	if err != nil {
 		return fmt.Errorf("failed to create file for: %v", err.Error())
 	}
-	resp, err := http.Get(url)
-	if err != nil {
-		return fmt.Errorf("download failed: %v", err.Error())
-	}
-	if resp.StatusCode != 200 {
-		return fmt.Errorf("download of %q failed with status %d: %q", url, resp.StatusCode, resp.Status)
-	}
-	defer resp.Body.Close()
 	_, err = io.Copy(out, resp.Body)
 	if err != nil {
 		return fmt.Errorf("failed to download from %q to %q: %v", url, localPath, err.Error())
